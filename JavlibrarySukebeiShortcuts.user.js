c33a7bd2a309da4be37d1c68cbe98e986e15704d
Runisco <runisco@gmail.com>
2022-03-19 17:18:26 +0100

    added more matches

--- JavlibrarySukebeiShortcuts.user.js
+++ JavlibrarySukebeiShortcuts.user.js
@@ -1,13 +1,34 @@
 // ==UserScript==
 // @name         Javlibrary sukebei shortcuts
 // @namespace    https://github.com/Runisco
-// @version      1.1
+// @version      1.0
 // @updateURL https://github.com/Runisco/JavlibrarySukebeiShortcuts/raw/main/JavlibrarySukebeiShortcuts.user.js
 // @downloadURL https://github.com/Runisco/JavlibrarySukebeiShortcuts/raw/main/JavlibrarySukebeiShortcuts.user.js
 // @supportURL https://github.com/Runisco/JavlibrarySukebeiShortcuts/issues
 // @description  Adds a shortcut to the toolbar of videos on Javlibrary to automatically search for the video on Sukebei
 // @author       Runisco
-// @match        https://www.javlibrary.com/en/vl_genre.php?g=*
+// @match https://www.javlibrary.com/en/vl_genre.php?g=*
+// @match https://www.javlibrary.com/en/vl_genre.php?&mode=&g=*&page=*
+// @match https://www.javlibrary.com/en/vl_update.php
+// @match https://www.javlibrary.com/en/vl_update.php?&mode=&page=*
+// @match https://www.javlibrary.com/en/vl_newrelease.php
+// @match https://www.javlibrary.com/en/vl_newrelease.php?&mode=&page=*
+// @match https://www.javlibrary.com/en/vl_newentries.php
+// @match https://www.javlibrary.com/en/vl_newentries.php?&mode=&page=*
+// @match https://www.javlibrary.com/en/vl_mostwanted.php
+// @match https://www.javlibrary.com/en/vl_mostwanted.php?&mode=&page=*
+// @match https://www.javlibrary.com/en/vl_bestrated.php
+// @match https://www.javlibrary.com/en/vl_bestrated.php?&mode=&page=*
+// @match https://www.javlibrary.com/en/vl_star.php?s=*
+// @match https://www.javlibrary.com/en/vl_star.php?&mode=&s=*&page=*
+// @match https://www.javlibrary.com/en/vl_maker.php?m=*
+// @match https://www.javlibrary.com/en/vl_maker.php?&mode=&m=*&page=*
+// @match https://www.javlibrary.com/en/vl_label.php?l=*
+// @match https://www.javlibrary.com/en/vl_label.php?&mode=&l=*&page=*
+// @match https://www.javlibrary.com/en/vl_searchbytitle.php?searchid=*&keyword=*
+// @match https://www.javlibrary.com/en/vl_searchbytitle.php?&searchid=*&page=*
+// @match https://www.javlibrary.com/en/vl_director.php?d=*
+// @match https://www.javlibrary.com/en/vl_director.php?d=*&page=*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // @require http://code.jquery.com/jquery-latest.js
